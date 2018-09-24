664f96c0fbba19ba5374dfeadf2fc6f2f628bd80
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-09-24 12:31:07 +0700

    Rename leftover naming refactor (#11)

--- scripts/ci-create-plan-artifact.sh
+++ scripts/ci-create-plan-artifact.sh
@@ -9,7 +9,7 @@ jq -n "{
     PR_ID: \"$PR_ID\",
     GIT_MASTER_COMMIT_ID: \"$GIT_MASTER_COMMIT_ID\",
     TF_WORKING_DIR: \"$TF_WORKING_DIR\"
-}" > build/metadata.json
+}" > artifact/metadata.json
 # Zip artifact folder
 cd artifact
 zip -r ../$${GIT_MASTER_COMMIT_ID}-${PR_ID}.zip .
--- scripts/ci-setup-env-var.sh
+++ scripts/ci-setup-env-var.sh
@@ -13,13 +13,13 @@ export PR_ID="$(ls .git/pr)"
 echo "PR_ID=${PR_ID}"
 export GIT_MASTER_COMMIT_ID="$(git rev-parse origin/master)"
 echo "GIT_MASTER_COMMIT_ID=${GIT_MASTER_COMMIT_ID}"
-#Get Temporary Github Token By using Github app private key from AWS Parameter Store
+# Get Temporary Github Token By using Github app private key from AWS Parameter Store
 gen-github-token.py
 export GITHUB_TOKEN=$(cat GITHUB_TOKEN)
 echo "GITHUB_TOKEN is set"
 export GIT_ASKPASS="parse-git-auth.sh"
 echo "GIT_ASKPASS=${GIT_ASKPASS}"
-#Get latest-commit-apply from artifact S3 Bucket
+# Get latest-commit-apply from artifact S3 Bucket
 aws s3 cp s3://${ARTIFACT_BUCKET}/latest-commit-apply latest-commit-apply
 export LATEST_COMMIT_APPLY="$(cat latest-commit-apply)"
 echo "LATEST_COMMIT_APPLY=${LATEST_COMMIT_APPLY}"
