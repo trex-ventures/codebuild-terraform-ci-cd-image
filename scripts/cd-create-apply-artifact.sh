47a91431ac66b19c4ab467b171f59776f9e830c5
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-09-26 06:53:55 +0700

    Fix cd-create-apply-artifact script (#16)

--- scripts/cd-create-apply-artifact.sh
+++ scripts/cd-create-apply-artifact.sh
@@ -2,9 +2,9 @@
 # Create Apply Artifact
 
 # Create new metadata.json
-jq ￼". + {
+jq ". + {
     GIT_COMMIT_ID: \"$GIT_COMMIT_ID\"
-    }" artifact/metadata.json > artifact/metadata-apply.json
+}" artifact/metadata.json > artifact/metadata-apply.json
 mv artifact/metadata-apply.json artifact/metadata.json
 
 # Create artifact
