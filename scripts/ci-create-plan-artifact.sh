ec8367b124bfddf7da742aecd296078d7ad81d17
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-09-26 09:58:27 +0700

    Fix wrong conditional (#20)

--- scripts/ci-create-plan-artifact.sh
+++ scripts/ci-create-plan-artifact.sh
@@ -3,7 +3,7 @@
 
 # Copy Terraform working directory
 mkdir -p artifact/${TF_WORKING_DIR}
-if [ ! -d "$TF_WORKING_DIR" ]; then
+if [ "$TF_WORKING_DIR" != "" ]; then
     cp -r ${TF_WORKING_DIR}/* artifact/${TF_WORKING_DIR}
 fi
 # Create metadata.json file
